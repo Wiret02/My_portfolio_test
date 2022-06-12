6104f4b548639944d5f4e3b8e90bce6511b9800d
Yukhym Rybenchuk <60313835+Wiret02@users.noreply.github.com>
2022-06-12 15:06:06 +0300

    Add files via upload

---
+++ pulse/js/script.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+
+    // Carousel Slick
+
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true 
+                }
+            }
+        ]
+    });
+
+    //Tabs
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+    
+    // Form Validation by jQuery Validation Plugin
+    function validateForm(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуста введите свое имя",
+                phone: "Пожалуста введите сой номер телефона",
+                email: {
+                    required: "Пожалуста введите вашу електронную почту",
+                    email: "Неправельно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForm('#consultation-form');    
+    validateForm('#consultation form');    
+    validateForm('#order form');    
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn();
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smooth scroll up
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1210) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offSet().top+"px"});
+        return false
+    });
+
+    new WOW().init();
+
+  });
